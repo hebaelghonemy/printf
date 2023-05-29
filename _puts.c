ef95ea5f0917ca9907afd03688c1438dd66d4641
mona <monasief833@gmail.com>
2023-05-29 15:42:18 -0700

    edit puts

--- _puts.c
+++ _puts.c
@@ -14,7 +14,7 @@ return (str - a);
 }
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to ptint
+ * @c: character to ptint
  *
  * Return: on success 1
  * on error, -1 is returned
